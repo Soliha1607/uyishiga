5351fdadf0836ad54f243dc2890cceeb416009f5
Soliha <solihapahridinova@gmail.com>
2024-10-19 00:52:13 +0500

    Update bilmiman.py

--- bilmiman.py
+++ bilmiman.py
@@ -2,3 +2,4 @@ something what i know but i do not know
 second commit i know it is not good but not bad 
 third one, it is not end
 and forth one, i don't know what i need to do
+last commit and that is end 
