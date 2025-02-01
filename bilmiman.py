3aa16ec9ce20753efa772081d1c3cb201f2cf244
Soliha <solihapahridinova@gmail.com>
2025-02-01 14:41:11 +0500

    Update bilmiman.py

--- bilmiman.py
+++ bilmiman.py
@@ -1,10 +1,3 @@
-something what i know but i do not know
-second commit i know it is not good but not bad 
-third one, it is not end
-and forth one, i don't know what i need to do
-last commit and that is end 
-
-
 Cinnamon in my teeth
 From your kiss
 You're touching me
